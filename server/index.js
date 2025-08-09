90bdd195c7cf06ef3325b11c4e6fc7d10501346a
shreyasKU31 <shreyasku31@gmail.com>
2025-08-09 20:40:25 +0530

    config dotenv

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import connectDB from "./config/db.js";
