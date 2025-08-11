1503dd27e314a20f7058f0fbccc6678b87aef817
shreyasKU31 <shreyasku31@gmail.com>
2025-08-11 16:45:17 +0530

    add user model

---
+++ server/schema/user.js
@@ -0,0 +1,43 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true, // Ensures no two users have the same Clerk ID
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    headline: {
+      type: String,
+      default: "Aspiring Developer", // A default value for new users
+    },
+    bio: String, // A simple string field
+    imageUrl: String,
+    // projects: [
+    //   {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "Project", // Creates a relationship to a 'Project' model
+    //   },
+    // ],
+  },
+  {
+    // This option automatically adds `createdAt` and `updatedAt` fields
+    timestamps: true,
+  }
+);
+
+const User = new model("user", userSchema);
+export default User;
