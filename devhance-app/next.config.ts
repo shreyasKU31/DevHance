da34b23a98194a4cbb72a024eda38c22e780f9b3
shreyasKU31 <shreyasku31@gmail.com>
2025-08-21 19:37:16 +0530

    add cors

--- devhance-app/next.config.ts
+++ devhance-app/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  allowedDevOrigins: ["https://385a84127d3176fe0e5721f5bee40a3b.serveo.net"],
 };
 
 export default nextConfig;
