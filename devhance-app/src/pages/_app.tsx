9ebb8886fcfee3bfb5beb03624f6630241e1f091
shreyasKU31 <shreyasku31@gmail.com>
2025-08-22 09:16:23 +0530

    remove clerk template

--- devhance-app/src/pages/_app.tsx
+++ devhance-app/src/pages/_app.tsx
@@ -1,22 +1,10 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
       <Component {...pageProps} />
     </ClerkProvider>
   );
